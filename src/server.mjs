f7749bc2ad8ec807c5300e7d84e5e40187a24ad2
Eduardo Souza <denis.souzadeoliveira05@gmail.com>
2023-08-01 22:43:45 -0300

    Removendo dados sensiveis

--- src/server.mjs
+++ src/server.mjs
@@ -1,32 +1,3 @@
-// import { createClient } from '@supabase/supabase-js'
-// const supabaseUrl = 'https://vtdnjwhphkxsgfculrsn.supabase.co'
-// const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ0ZG5qd2hwaGt4c2dmY3VscnNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc5NjU0NjUsImV4cCI6MjAwMzU0MTQ2NX0.cQaYd3JY-5C39T9WCVKNBzP3ymJvBImh8eqPHzwsmcw"
-// const supabase = createClient(supabaseUrl, supabaseKey)
-
-
-
-// async function executeQuery() {
-//     try {
-//       const { data, error } = await supabase
-//         .from('tabela_de_teste')
-//         .insert([
-//           { nome: 'someValue', email: 'otherValue' },
-//         ])
-//         .select();
-      
-//       if (error) {
-//         console.error(error);
-//       } else {
-//         console.log(data);
-//       }
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   }
-  
-//   executeQuery();
-
-
 const app = express();
 
 app.use(express.json());
